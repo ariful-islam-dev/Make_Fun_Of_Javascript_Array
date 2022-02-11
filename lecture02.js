f0f4357d635a317cd2e3677e5e39c7d27f9366eb
nodeAriful <ariful.node82@gmail.com>
2022-02-11 20:47:45 +0600

    build won map mathods

--- lecture02.js
+++ lecture02.js
@@ -12,37 +12,56 @@ let names = [
 
 // why us map
 
-function reuse(arr, cb) {
+// function reuse(arr, cb) {
+//   for (let i = 0; i < arr.length; i++) {
+//     cb(arr[i], i, arr);
+//   }
+// }
+
+// // Example One
+// const result1 = [];
+// reuse(names, (value) => {
+//   result1.push(value.length);
+// });
+
+// console.log(result1);
+// // Example Two
+
+// const result2 = [];
+// reuse(names, (value) => {
+//   result2.push(value.toUpperCase());
+// });
+
+// console.log(result2);
+// // Example three
+
+// const result3 = [];
+// reuse(names, (value) => {
+//   result3.push(value.substr(9));
+// });
+
+// console.log(result3);
+
+function reuseAndMap(arr, cb) {
+  const result = [];
   for (let i = 0; i < arr.length; i++) {
-    cb(arr[i], i, arr);
+    const r = cb(arr[i], i, arr);
+    result.push(r);
   }
+  return result;
 }
 
-// Example One
-const result1 = [];
-reuse(names, (value) => {
-  result1.push(value.length);
-});
+const length = reuseAndMap(names, (value) => value.length);
+const uppers = reuseAndMap(names, (value) => value.toUpperCase());
+const sliced = reuseAndMap(names, (value) => value.substr(9));
 
-console.log(result1);
-// Example Two
+console.log(length, uppers, sliced);
 
+const length1 = names.map((value) => value.length);
+const uppers1 = names.map((value) => value.toUpperCase());
+const sliced1 = names.map((value) => value.substring(9));
 
-const result2 = [];
-reuse(names, (value) => {
-  result2.push(value.toUpperCase());
-});
-
-console.log(result2);
-// Example three
-
-const result3 = [];
-reuse(names, (value) => {
-  result3.push(value.substr(9));
-});
-
-console.log(result3);
-
+console.log(length1, uppers1, sliced1);
 const studentArray = [
   {
     id: 1,
