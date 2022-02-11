2e83fb7fa81c40b4948c68ba57b21636dc854514
nodeAriful <ariful.node82@gmail.com>
2022-02-11 21:13:01 +0600

    using fiter

---
+++ lecture03.js
@@ -0,0 +1,43 @@
+const studentArray = [
+  {
+    id: 1,
+    name: "Ariful Islam Raju",
+    gpa: 2.99,
+    email: "arifulislam@gmail.com",
+    due: true,
+    dueAmount: 1000,
+  },
+  {
+    id: 2,
+    name: "Aysha Mone",
+    gpa: 4.19,
+    email: "ayshaArif@gmail.com",
+    due: true,
+    dueAmount: 0,
+  },
+  {
+    id: 3,
+    name: "Abdullah Al Zarif",
+    gpa: 5.0,
+    email: "zrif@gmail.com",
+    due: true,
+    dueAmount: 1300,
+  },
+  {
+    id: 4,
+    name: "Asadullah Al Labib",
+    gpa: 5.0,
+    email: "labib@gmail.com",
+    due: true,
+    dueAmount: 995,
+  },
+  {
+    id: 5,
+    name: "Sadiqul Islam Jamil",
+    gpa: 5.0,
+    email: "jamil@gmail.com",
+     
+    due: true,
+    dueAmount: 1100,
+  },
+];
