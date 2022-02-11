4c00224beeca4d1587fc3164ccb1e8796480af79
ariful4082 <ariful4082@gmail.com>
2022-02-11 12:31:13 +0600

    create stack constractor function and methods

--- README.md
+++ README.md
@@ -2,10 +2,4 @@
 
 **_Stack Learner_**
 
-- [x] Array CRUD Oparation
-  - [ ] Insert Element
-  - [ ] Update Eleemnt
-  - [ ] Deletement Element
-- [x] Array Building Method
-  - [ ] array.concate();
-  - [ ] array.slice()
+- [x] Stack Data Stracture
--- lecture02.js
+++ lecture02.js
@@ -1,31 +1,40 @@
 /**
- * @Title : Update Existing Elements
+ * @Title : Implement a basic stack
  */
 
-// Easy One
-const numbers = [1, 2, 7, 4, 5];
-numbers[2] = 3;
-console.log(numbers);
+const MAX_SIZE = 30;
 
-
-// Array of Objects
-const students = [
-    {id: 1, name: "Ariful"},
-    {id: 2, name: "Aysha"},
-    {id: 3, name: "Zarif"},
-    {id: 4, name: "Labib"},
-    {id: 5, name: "Jamil"},
-];
-
-const givenID = 3;
-const updatedName = "Assadullah Al Labib";
-
-for (let i = 0; i < students.length; i++) {
-    if(givenID === students[i].id){
-        students[i].name = updatedName;
-        break
+class Stack {
+  constructor() {
+    this.list = new Array(MAX_SIZE);
+    this.top = -1;
+  }
+  push(item) {
+    if (this.top >= MAX_SIZE) {
+      console.log("Stack Overflow");
+      return false;
     }
-    
+    this.list[++this.top] = item;
+    return true;
+  }
+  pop() {
+      if(this.isEmpty()){
+          console.log("Stack Underflow");
+          return false
+      }
+      let item = this.list[this.top];
+      delete this.list[this.top];
+      this.top--;
+      return item
+  }
+  peek() {
+    if (this.isEmpty()) {
+      console.log("Stack Under Flow");
+      return false;
+    }
+    return this.list[this.top];
+  }
+  isEmpty() {
+    return this.top < 0;
+  }
 }
-
-console.log(students);
