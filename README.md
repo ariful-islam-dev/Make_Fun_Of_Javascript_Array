2276e0957d01a4a5fac563e49a92b58886fd8aba
ariful4082 <ariful4082@gmail.com>
2022-02-13 01:42:17 +0600

    basic theory of reduce

--- README.md
+++ README.md
@@ -2,9 +2,4 @@
 
 **_Stack Learner_**
 
-- [x] Array Looping Methods
-  - [ ] Array forEach();
-  - [ ] Array map();
-  - [ ] Array filter();
-  - [ ] Array filter() & map();
-  - [ ] Array every() and some()
+- [x] Reduce of Array
--- lecture01.js
+++ lecture01.js
@@ -1,65 +1,29 @@
-let names = [
-  "Ariful Islam Raju",
+/**
+ * @title :  Understand the comcept of Reduce
+ */
+
+// Reduce  - make smaller or less in amount, degree, or size
+const arr = [1, 2, 3, 4, 5];
+
+// reduced = 15;
+// reduced = 3;
+
+const names = [
+  "Ariful Islam",
   "Aysha Mone",
-  "Abdullah Al Zarif",
-  "Assadullah Al Labib",
+  "Abdullah Al-Zarif",
+  "Asadullah Al-Labib",
+];
+// reduced [{first: 'Ariful', last: 'Nayem'}{'first: 'Aysha', last: 'Mone'}]
+
+const votes = [
+  "Java",
+  "Java",
+  "Python",
+  "Java",
+  "Javascript",
+  "Python",
+  "Javascript",
 ];
 
-// How to do it on For loop
-
-// for (let i = 0; i < names.length; i++) {
-//   //   console.log(names[i]);
-// }
-
-// for (let i = 0; i < names.length; i++) {
-//   //   console.log(`${i + 1}: ${names[i]}`);
-// }
-
-// // Iterating Logic -> Bussiness Logic
-
-// function reuse(arr, logic) {
-//   for (let i = 0; i < arr.length; i++) {
-//     // const element = arr[i];
-//     logic(arr[i], i, arr);
-//   }
-// }
-
-// function logic(value, index, arr) {
-//   console.log(value, index, arr);
-// }
-
-// function log(value){
-// console.log(value);
-// }
-
-// reuse([]);
-// reuse([1, 2, 3], log);
-// reuse(["One", "Two"], logic);
-
-function reuse(arr, cb) {
-  for (let i = 0; i < arr.length; i++) {
-    const element = arr[i];
-    cb(arr[i], i, arr);
-  }
-}
-
-const nums = [1, 2, 3, 4];
-let sum = 0;
-
-function sumFunc(v) {
-  sum += v;
-}
-reuse(nums, function (v) {
-  sum += v;
-});
-console.log(sum);
-
-// for name array
-reuse(names, (value, index) => {
-  console.log(`${index + 1}: ${value}`);
-});
-
-// forEach Method
-names.forEach((value, index) => {
-  console.log(`[Array] ${index + 1} : ${value}`);
-});
+// reduces { java: 3, Python: 2, Javascript: 2}
