1cfac595fdf39500e02c2673097a6da9b9ba2c4f
ariful4082 <ariful4082@gmail.com>
2022-02-13 12:30:15 +0600

    react array single objec

---
+++ lecture09.js
@@ -0,0 +1,29 @@
+/**
+ * @title: Reduce Array into Single Objce
+ */
+
+const products = [
+  { name: "Javascript Cookbook", price: 350 },
+  { name: "Head First Python", price: 200 },
+  { name: "Head First Java", price: 400 },
+  { name: "Javascript Cookbook", price: 350 },
+  { name: "Head First Java", price: 400 },
+  { name: "Javascript Cookbook", price: 350 },
+  { name: "Javascript Cookbook", price: 350 },
+  { name: "Head First C", price: 300 },
+];
+
+const invoice = products.reduce((acc, cur) => {
+  if (acc[cur.name]) {
+    acc[cur.name].quantity++;
+    acc[cur.name].price += cur.price;
+  } else {
+    acc[cur.name] = {
+      price: cur.price,
+      quantity: 1,
+    };
+  }
+  return acc;
+}, {});
+
+console.log(invoice);
