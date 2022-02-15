2ba89abb817ec3bf524286bd6ffa30d2d63e39d9
ariful4082 <ariful4082@gmail.com>
2022-02-15 07:46:19 +0600

    array join method

---
+++ lecture02.js
@@ -0,0 +1,10 @@
+/**
+ * @title : Array Join Method
+ * 
+ */
+
+const a = ['Stack', 'Learner', 'And', 'Stack', 'School'];
+
+console.log(a.join(' '));
+console.log(a.join());
+console.log(a.join(', '));
