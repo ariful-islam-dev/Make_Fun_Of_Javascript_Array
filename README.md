a04cb29d9766d640a99bb524dffd097c43617d4e
ariful4082 <ariful4082@gmail.com>
2022-02-15 08:05:15 +0600

    example of iterator

--- README.md
+++ README.md
@@ -2,10 +2,5 @@
 
 **_Stack Learner_**
 
-- [x] Array Building Methrods
-  - [ ] Array Constactor
-    1.  Array.from()
-    2.  Array.isArray()
-    3.  Array.of()
-  - [ ] array.join()
-  - [ ] includes, indexOf and LastIndexOf
+- [x] Array Itarable
+  - [ ]
--- lecture01.js
+++ lecture01.js
@@ -1,80 +1,35 @@
 /**
- * @title: Static Methods Vs Instance Methods
+ * @title: Understand Iterable Object
  */
 
-const arr = [];
-// arr.concat();
-// Array.from();
+function makeRangeIterator(start = 0, end = Infinity, step = 1) {
+  let nextIndex = start;
+  let iterationCount = 0;
 
-//Example Of Javascript Method
-// Array.from();
+  const rangeIterator = {
+    next: function () {
+      let result;
+      if (nextIndex < end) {
+        result = { value: nextIndex, done: false };
+        nextIndex += step;
+        iterationCount++;
+        return result;
+      }
+      return { value: iterationCount, done: true };
+    },
+  };
 
-const s = Array.from("Stack Learner");
-// console.log(s);
-
-const set = new Set(["foo", "bar", "baz", "foo"]);
-const s1 = Array.from(set);
-// console.log(s1);
-
-const mapper = new Map([
-  ["1", "a"],
-  ["2", "b"],
-]);
-Array.from(mapper.values());
-Array.from(mapper.keys());
-
-function f() {
-  return Array.from(arguments);
+  return rangeIterator;
 }
 
-f(1, 2, 3);
 
-Array.from([1, 2, 3], (x) => x + x);
+const it = makeRangeIterator(1,10,2);
+let result = it.next()
 
-Array.from({ length: 5 }, (v, i) => i);
 
-const range = (start, stop, step) => {
-  Array.from(
-    {
-      length: (stop - start) / step + 1,
-    },
+while(!result.done){
+    console.log(result.value);
+    result = it.next()
+}
 
-    (_, i) => start + i * step
-  );
-};
-
-range(0, 4, 1);
-
-range(1, 10, 2);
-
-range("A".charCodeAt(0), "Z".charCodeAt(0), 1).map((x) =>
-  String.fromCharCode(x)
-);
-//Array.isArray();
-
-Array.isArray([]);
-Array.isArray([1]);
-Array.isArray(new Array());
-Array.isArray(new Array("a", "b", "c", "d"));
-Array.isArray(new Array(3));
-Array.isArray(Array.prototype);
-
-// all fllowing callse return false
-Array.isArray();
-Array.isArray({});
-Array.isArray(null);
-Array.isArray(undefined);
-Array.isArray(17);
-Array.isArray("Array");
-Array.isArray(true);
-Array.isArray(false);
-Array.isArray(new Uint8Array(32));
-Array.isArray({ __proto__: Array.prototype });
-
-// Array.of();
-
-Array.of(7);
-Array.of(1, 2, 3);
-Array.of(undefined);
-Array(7);
-Array(1, 2, 3);
+console.log('Iteration over', result.value);
