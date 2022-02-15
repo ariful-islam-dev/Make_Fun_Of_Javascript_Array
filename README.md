e4ffc956f3cf4ae9c43fd3bd641b5f0b475d538e
ariful4082 <ariful4082@gmail.com>
2022-02-15 14:34:22 +0600

    array prototype object

--- README.md
+++ README.md
@@ -2,8 +2,4 @@
 
 **_Stack Learner_**
 
-- [x] Array is an Object
-  - [ ] Array is an Objcet
-  - [ ] Copy Array Element
-    1. Shallow Copy
-    2. Deep Copy
+- [x] Array Prototype
--- lecture01.js
+++ lecture01.js
@@ -4,25 +4,31 @@
  */
 
 const arr = [1, 2, 3];
-// console.log(typeof arr);
-// console.log(arr.constructor);
-// console.log(arr.constructor.prototype);
 
-arr.test = "Test Property";
-arr.log = function () {
-  console.log(this);
-  console.log(this.test);
-  console.log(this.length);
-  console.log(this[0]);
+console.log(arr.constructor.prototype);
+console.log(Array.prototype);
+console.log(Object.getOwnPropertyNames(Array.prototype));
+
+Array.prototype.log = function () {
+  console.log(this.join(" | "));
 };
 
 arr.log();
 
-const obj = {};
-console.log(typeof obj);
+const names = ["Ariful", "Johirul", "Azizul"];
 
-console.log(arr instanceof Array);
-console.log(obj instanceof Array);
-console.log(Array.isArray(arr));
-console.log(Object.prototype.toString.call(arr) === "[object Array]");
-console.log(Object.prototype.toString.call(arr));
+names.log();
+
+Array.prototype.loop = function (cb) {
+  for (let i = 0; i < this.length; i++) {
+    cb(this[i], i, this);
+  }
+};
+
+names.loop((v) => {
+  console.log(` My Name Is ${v}`);
+});
+
+arr.loop((v) => {
+  console.log(` V = ${v} and V*2 = ${v * 2}`);
+});
